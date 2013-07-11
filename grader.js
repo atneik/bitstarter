42d99ecb756f9eb6bc15951c4af35a13122c7148
Ubuntu <ubuntu@ip-172-31-16-7.ap-southeast-1.compute.internal>
2013-07-11 19:28:33 +0000

    updated grader.js with url option

--- grader.js
+++ grader.js
@@ -1,33 +1,13 @@
 #!/usr/bin/env node
-/*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
-
+var util = require('util');
 var fs = require('fs');
 var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "https://github.com/atneik/bitstarter/blob/c795d78e728cebaa18e42d384e17fd83d2907f2d/index.html";
+var URL_DEFAULT = "http://warm-mesa-7096.herokuapp.com/";
+var flag = 0;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -38,9 +18,35 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertUrlExists = function(url) {
+  return url;
 };
+
+var cheerioHtmlFile = function(htmlfile) {
+    var data = fs.readFileSync(htmlfile);
+	var hh = cheerio.load(fs.readFileSync(htmlfile));
+	//console.log(hh);
+	return hh;
+};
+
+var cheerioUrl = function(url, checksfile) {
+	var dataString;
+	var flag = 0;
+	rest.get(url).on('complete',function(result){
+		if (result instanceof Error){
+			console.log('Error' +result.message);
+			this.retry(5000);
+		}else{
+
+		var checkJson =	 checkUrl(cheerio.load(result), checksfile);	 
+
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+
+		}
+	});
+};
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -48,14 +54,33 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+//console.log($);    
+var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+//console.log($(checks[ii]));        
+out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var dummyF = function(dataString){
+return dataString;
+};
+
+var checkUrl = function(dataString, checksfile) {
+    $ = dummyF(dataString);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+//console.log($(checks[ii]));        
+out[checks[ii]] = present;
     }
     return out;
 };
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -67,11 +92,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_link>', 'Link to index.html', clone(assertLinkExists), URL_DEFAULT)
+        .option('-u, --url <url_link>', 'Url to index.html', clone(assertUrlExists), URL_DEFAULT)
 	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+   var outJson = JSON.stringify(checkJson, null, 4);
+   console.log(outJson);
+
+     cheerioUrl(program.url, program.checks);
+   } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkUrl = checkUrl;
 }
