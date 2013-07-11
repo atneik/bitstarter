12ebb1f88044ab95b1eb230473d60477b069a4ab
Ubuntu <ubuntu@ip-172-31-16-7.ap-southeast-1.compute.internal>
2013-07-11 10:33:22 +0000

    web.js modified

--- web.js
+++ web.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var fs = require('fs');
+var util = require('util');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  	var buff = new Buffer(256);
-	var len=256;
-	var ctr = 0;
-	do{
-		len  = buff.write(fs.readFileSync('index.html','utf-8').slice(ctr,ctr+len),'utf-8');
-		ctr = ctr + len;
-		response.send(buff.toString());
-		buff.fill(' ');
-	}while(len!=0);
+  	var stats = fs.statSync('index.html');
+	var buff = new Buffer(stats.size);
+	buff.write(fs.readFileSync('index.html','utf-8'));
+	response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
