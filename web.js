960a5eb674adc0374f4a2260427a4049adba354d
Ubuntu <ubuntu@ip-172-31-16-7.ap-southeast-1.compute.internal>
2013-07-11 05:52:30 +0000

    web.js reading from index.html 5

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   	var buff = new Buffer(50);
-	var len = buff.write(fs.readFileSync('index.html','utf-8');
+	var len = buff.write(fs.readFileSync('index.html','utf-8'));
 	response.send(buff.toString('utf-8', 0, len));
 });
 
