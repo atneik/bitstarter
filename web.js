7731705e9333572aa919fd7ae82f435fd0e86402
Aniket Handa <atneik@gmail.com>
2013-08-20 20:20:47 +0000

    express fix

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var util = require('util');
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
   	var stats = fs.statSync('index.html');
